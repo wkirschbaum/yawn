e9c9f9a83413c5e82117900d7f6870b7698e1e58
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2016-01-05 09:10:40 +0200

    renamed yawn file to main

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 	"os"
 )
 
-func ListenAndServer(handler http.Handler) {
+func ListenAndServe(handler http.Handler) {
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = "3000"
 	}
 	addr := ":" + port
 	log.Println("Starting server on " + addr)
-	http.ListenAndServe(addr, handler)
+	err := http.ListenAndServe(addr, handler)
+	if err != nil {
+		panic(err)
+	}
 }
